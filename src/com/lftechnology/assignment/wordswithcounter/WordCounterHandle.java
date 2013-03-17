1abc70bcf6c743ed567326d69e5ec6f82d1aeec1
syanjo <syanjo@syanjo-Studio-1569>
2013-03-17 18:28:59 +0545

    Improving Java Documentation

--- src/com/lftechnology/assignment/wordswithcounter/WordCounterHandle.java
+++ src/com/lftechnology/assignment/wordswithcounter/WordCounterHandle.java
@@ -7,11 +7,30 @@ package com.lftechnology.assignment.wordswithcounter;
 import java.io.IOException;
 
 /**
+ * It is
+ * <code>WordCounterHandle class</code> for word count and store them into
+ * another file by sorting them into decreasing order <br>
  *
  * @author syanjo
+ *
  */
 public class WordCounterHandle {
 
+    /**
+     * We have a input txt file which contains some information <br>
+     * This WordCounterHandle takes the name of input txt file as *.txt <br>
+     * and then stores them into
+     * <code>ArrayList of WordWithCounter</code><br>
+     * using
+     * <code>ArrayList of type String</code>, Which is used as a temporary
+     * holder of those all words;<br>
+     * <code>Sorting operation </code> is performed Finally top five word are
+     * stored in another txt file.
+     * @param agrs
+     * @throws IOException
+     * @see com.lftechnology.assignment.wordswithcounter.WordWithCounter
+     * @see com.lftechnology.assignment.wordswithcounter.WordCounter
+     */
     public static void main(String[] agrs) throws IOException {
         WordCounter wordCounter = new WordCounter();
         wordCounter.addWordInArrayList("inputFile.txt");
--- src/com/lftechnology/assignment/wordswithcounter/WordWithCounter.java
+++ src/com/lftechnology/assignment/wordswithcounter/WordWithCounter.java
@@ -1,19 +1,39 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package com.lftechnology.assignment.wordswithcounter;
 
 /**
- *
+ *This <code>WordWithCounter </code>class  holds two data; word and its word count
+ * <br> The <code>WordWithCounter</code> constructor does initialization of count equal to 1 of 
+ * that related word.
  * @author syanjo
  */
 class WordWithCounter {
+    /**
+     * This <code>word</code> of <code>String </code> datatype holds word from<br>
+     * file.
+     */
     public String word;
+    /**
+     * This <code>wordCount</code> of <code> Integer</code> datatype holds count of <br>
+     * every words; Intially its value set into 1 by constructor but after all its <br>
+     * value is increases it same word is repeated in multiple time on that file. 
+     * 
+     */
     public int wordCount;
-
+/**
+ * This <code>WordWithCounter</code> is constructor.
+ * @param word 
+ * @see com.lftechnology.assignment.wordswithcounter.WordCounterHandle 
+ * @see com.lftechnology.assignment.wordswithcounter.WordCounter
+ */
     public WordWithCounter(String word) {
     this.word = word;
+    /**
+     * wordCount is set to 1; The increment is done in another <code>class WordCounter</code>.
+     * 
+     */
     wordCount =1;
     }
     
